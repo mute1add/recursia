f215abefbc3e6d1fa78d108ab29314f0cfe7f8bf
Roma <de11romik@gmail.com>
2017-03-12 14:17:40 +0000

    final

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 $(function() {
     new WOW().init();
 });
+function adjustHeight(el){
+    el.style.height = (el.scrollHeight > el.clientHeight) ? (el.scrollHeight)+"px" : "60px";
+}
+$(function() {
+   $('#bs').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/de11romik@gmail.com", 
+            method: "POST",
+            data: {
+                firstName:    $('#name').val(),
+                lastName:     $('#sname').val(),
+                comment:      $('#text').val()
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form').html('<h1>I appreciate your opinion!</h1><p>Thank you for feedback</p>')
+        });
+   }); 
+});
